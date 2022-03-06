7c6ed372d42df88960bf2b92f052fa5b892e87b0
rajrachchh14 <rajrachchh14@gmail.com>
2022-03-06 20:18:49 +0530

    1 file modified

--- Component/Reusable/HeaderLayout.tsx
+++ Component/Reusable/HeaderLayout.tsx
@@ -8,9 +8,9 @@ function HeaderLayout() {
       <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
         <div className="logo" />
         <Menu theme="dark" mode="horizontal">
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+          <Menu.Item key="1">Home</Menu.Item>
+          <Menu.Item key="2">About</Menu.Item>
+          <Menu.Item key="3">Contact</Menu.Item>
         </Menu>
       </Header>
     </Layout>
