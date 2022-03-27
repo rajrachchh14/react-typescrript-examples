91f973bfbeaa9d9e9b21f836cd1c18ad0d5e5497
rajrachchh14 <rajrachchh14@gmail.com>
2022-03-27 21:39:03 +0530

    1 file modified, 2 added

--- App.tsx
+++ App.tsx
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 import List from './Examples/List';
 import { Layout, Menu } from 'antd';
 import SiteLayout from './Component/Reusable/SiteLayout';
+import PropsExample1 from './Examples/PropsExample1';
 const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <div>
       <SiteLayout>
-        <List />
+        <PropsExample1 name="raj" />
       </SiteLayout>
     </div>
   );
---
+++ Examples/a-skeleton.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+const PropsExample1 = () => {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  );
+};
+export default PropsExample1;
